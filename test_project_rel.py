e82b031054339b3c13ac8057353b1ceaf9836731
jakob-ra <jakob.rauch@student.uva.nl>
2022-02-03 17:46:33 +0100

    Update test_project_rel.py

--- test_project_rel.py
+++ test_project_rel.py
@@ -11,14 +11,14 @@ from scripts.rel_pipe import make_relation_extractor, score_relations
 from scripts.rel_model import create_relation_model, create_classification_layer, create_instances, create_tensors
 
 # We load the relation extraction (REL)
-nlp = spacy.load('en_core_web_trf', exclude=['tagger', 'parser', 'attribute_ruler', 'lemmatizer'])
-nlp.add_pipe('sentencizer', after='transformer')
-nlp.add_pipe('organization_extractor', after='ner')
-
-nlp_rel = spacy.load('training/model-best', vocab=nlp.vocab)
-nlp.add_pipe('transformer', name='rel_transformer', source=nlp_rel)
-nlp.add_pipe('relation_extractor', source=nlp_rel)
-nlp.component_names
+# nlp = spacy.load('en_core_web_trf', exclude=['tagger', 'parser', 'attribute_ruler', 'lemmatizer'])
+# nlp.add_pipe('sentencizer', after='transformer')
+# nlp.add_pipe('organization_extractor', after='ner')
+#
+# nlp_rel = spacy.load('training/model-best', vocab=nlp.vocab)
+# nlp.add_pipe('transformer', name='rel_transformer', source=nlp_rel)
+# nlp.add_pipe('relation_extractor', source=nlp_rel)
+# nlp.component_names
 
 nlp = spacy.load('training/model-best')
 
@@ -31,22 +31,22 @@ for doc in nlp.pipe(text):
         print([x for x in entry.items() if x[1] >= 0.9])
         [x for x in entry.items()]
 
-text = ['Microsoft Inc and Sun Microsystems just announced that they will break up.']
-
-kb = pd.io.json.read_json(path_or_buf='/Users/Jakob/Documents/Thomson_SDC/Full/SDC_training_dict.json',
-                          orient='records', lines=True)
-
-
-sdc = pd.read_pickle('/Users/Jakob/Documents/Thomson_SDC/Full/SDC_Strategic_Alliances_Full.pkl')
-
-
-def test_rel_project():
-    root = Path(__file__).parent
-    project_assets(root)
-    project_run(root, "all", capture=True)
-
-
-if __name__ == "__main__":
-    project_run(project_dir=Path.cwd(), subcommand='train_cpu')
+# text = ['Microsoft Inc and Sun Microsystems just announced that they will break up.']
+#
+# kb = pd.io.json.read_json(path_or_buf='/Users/Jakob/Documents/Thomson_SDC/Full/SDC_training_dict.json',
+#                           orient='records', lines=True)
+#
+#
+# sdc = pd.read_pickle('/Users/Jakob/Documents/Thomson_SDC/Full/SDC_Strategic_Alliances_Full.pkl')
+#
+#
+# def test_rel_project():
+#     root = Path(__file__).parent
+#     project_assets(root)
+#     project_run(root, "all", capture=True)
+#
+#
+# if __name__ == "__main__":
+#     project_run(project_dir=Path.cwd(), subcommand='train_cpu')
 
 
