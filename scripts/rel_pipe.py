54e575c10d7293105a93b1faeba773b516bcf0cb
jakob-ra <jakob.rauch@student.uva.nl>
2022-02-04 12:17:03 +0100

    Update rel_pipe.py

--- scripts/rel_pipe.py
+++ scripts/rel_pipe.py
@@ -219,6 +219,7 @@ def score_relations(examples: Iterable[Example], threshold: float) -> Dict[str, 
     # get labels from first example
     for example in list(examples)[0]:
         labels = list(list(example.reference._.rel.values())[0].keys())
+        print(labels)
     f_per_type = {label: PRFScore() for label in labels}
     for example in examples:
         gold = example.reference._.rel
