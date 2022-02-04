e3c13b4d9c81dc3deda5344b9b4259e8433828ec
jakob-ra <jakob.rauch@student.uva.nl>
2022-02-04 11:16:47 +0100

    progress bar evaluate

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import spacy
 from spacy.tokens import DocBin, Doc
 from spacy.training.example import Example
+from tqdm import tqdm
 
 # make the factory work
 from rel_pipe import make_relation_extractor, score_relations
@@ -26,7 +27,7 @@ def main(trained_pipeline: Path, test_data: Path, print_details: bool):
     doc_bin = DocBin(store_user_data=True).from_disk(test_data)
     docs = doc_bin.get_docs(nlp.vocab)
     examples = []
-    for gold in docs:
+    for gold in tqdm(docs):
         pred = Doc(
             nlp.vocab,
             words=[t.text for t in gold],
@@ -53,29 +54,30 @@ def main(trained_pipeline: Path, test_data: Path, print_details: bool):
                     continue
             print()
 
-    random_examples = []
-    docs = doc_bin.get_docs(nlp.vocab)
-    for gold in docs:
-        pred = Doc(
-            nlp.vocab,
-            words=[t.text for t in gold],
-            spaces=[t.whitespace_ for t in gold],
-        )
-        # pred.ents = gold.ents
-        relation_extractor = nlp.get_pipe("relation_extractor")
-        get_instances = relation_extractor.model.attrs["get_instances"]
-        for (e1, e2) in get_instances(pred):
-            offset = (e1.start, e2.start)
-            if offset not in pred._.rel:
-                pred._.rel[offset] = {}
-            for label in relation_extractor.labels:
-                pred._.rel[offset][label] = random.uniform(0, 1)
-        random_examples.append(Example(pred, gold))
+    # random baseline
+    # random_examples = []
+    # docs = doc_bin.get_docs(nlp.vocab)
+    # for gold in docs:
+    #     pred = Doc(
+    #         nlp.vocab,
+    #         words=[t.text for t in gold],
+    #         spaces=[t.whitespace_ for t in gold],
+    #     )
+    #     # pred.ents = gold.ents
+    #     relation_extractor = nlp.get_pipe("relation_extractor")
+    #     get_instances = relation_extractor.model.attrs["get_instances"]
+    #     for (e1, e2) in get_instances(pred):
+    #         offset = (e1.start, e2.start)
+    #         if offset not in pred._.rel:
+    #             pred._.rel[offset] = {}
+    #         for label in relation_extractor.labels:
+    #             pred._.rel[offset][label] = random.uniform(0, 1)
+    #     random_examples.append(Example(pred, gold))
 
     thresholds = [0.000, 0.050, 0.100, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99, 0.999]
-    print()
-    print("Random baseline:")
-    _score_and_format(random_examples, thresholds)
+    # print()
+    # print("Random baseline:")
+    # _score_and_format(random_examples, thresholds)
 
     print()
     print("Results of the trained model:")
